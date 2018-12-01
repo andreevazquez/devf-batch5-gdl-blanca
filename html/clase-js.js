cf9a9ed4e782078324655f7f8073674b43765c20
Ricardo Vazquez <ricardovazqueztovar@gmail.com>
2018-11-30 21:52:47 -0600

    clase de JS

---
+++ html/clase-js.js
@@ -0,0 +1,54 @@
+    var edad=18;
+    debugger;
+
+    if(edad>=18){
+    console.log('ya es mayor de edad');
+    if(edad>=18 & edad<=24){
+    }else{
+        console.log('Universidad');
+    if(edad>=25 & edad<=28){
+
+    }else{
+        console.log('Posgrado');
+    }if(edad>=29 & edad<=33){
+
+    }else{
+        console.log('Adulto Joven');
+    }if(edad>=34 & edad<=40){
+
+    }else{
+        console.log('Chavoruco');
+
+    }if(edad>=41 & edad<=47){
+
+    }else{
+        console.log('Señor Crisis');
+
+    }if(edad>=48 & edad<=60){
+
+    }else{
+        console.log('Viejo Lesbiano');
+
+    }if(edad>61){
+
+    }else{
+        console.log('Suggar Daddy');
+    
+    }
+
+        }
+    }else{
+    console.error('esta morro todavia');
+
+    if(edad>=6 & edad<=12){
+        console.log('Primaria');
+    }else{
+        if(edad>=13 & edad<=15){
+            console.log('Secundaria');
+        }else{
+            if(edad>=16 & edad<=17)
+            console.log('Preparatoria');
+        }
+    }
+}    
+
