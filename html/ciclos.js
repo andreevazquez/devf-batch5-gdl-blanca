038859b342110c3b5c30b07a9a74af5a0e5d3510
Ricardo Vazquez <ricardovazqueztovar@gmail.com>
2018-12-03 21:33:44 -0600

    practica de ciclos en JS

---
+++ html/ciclos.js
@@ -0,0 +1,24 @@
+/**
+ * Imprimir 100 veces el mensaje en consola
+
+
+for(conta = 0; conta < 10000; conta++){
+console.log(`Me canso ganzo ${conta} = ${conta*2}`);
+if(conta==7500)
+break;
+}
+**/
+for(i=0; i<=10; i++){
+    console.log(`Tabla del ${i}`);
+    for(x=1; x<=10; x++){
+        console.log(`${x} x ${i} = ${x*i}`);
+    }  
+}
+for(i=0; i<=100; i++){
+var elBoton=document.createElement('button');
+elBoton.textContent="el texto que se me pegue la gana";
+var elBody=document.getElementsByTagName('body') [0];
+elBody.appendChild(elBoton);
+}
+
+
