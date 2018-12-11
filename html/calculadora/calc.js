3a94201413e9b858eaab2bd20b7713c6fdab07a2
Ricardo Vazquez <ricardovazqueztovar@gmail.com>
2018-12-10 21:01:04 -0600

    correccion de sintaxis y raiz cuadrada

--- html/calculadora/calc.js
+++ html/calculadora/calc.js
@@ -1,10 +1,13 @@
 var primerNumero
 var operacion
 var segundoNumero
-
+var pantalla= document.getElementById('pantalla');
 function muestraValor(valor){
-    var pantalla=document.getElementById('pantalla'); // obteber el elemento en la "pantalla"
+    //pantalla.getElementById('pantalla'); // obteber el elemento en la "pantalla"
     var valorActual = pantalla.innerText; // el valor que al seleccionar el boton lo muestra en la pantalla
+    
+    if(valorActual.indexOf('.')>=0)
+      return 
     if (valorActual.length<14){ //es el total de espacios que tengo
         pantalla.innerText= `${valorActual}${valor}`;//Esto muestra y concatena la variable valor y valor actual
     }
@@ -14,34 +17,38 @@ function borrar(){
     valorActual.innerText='';//escribir "nada"
 }
 function guardar(signo){
-    primerNumero=document.getElementById('pantalla').textContent;//agarras el texto dentro del h3
+
+    primerNumero=Number(pantalla.textContent);//agarras el texto dentro del h3
     operacion=signo
     borrar()
-    console.log(primerNumero, segundoNumero)
+    console.log(primerNumero,segundoNumero)
 }
 function igualA(){
-    segundoNumero=document.getElementById('pantalla').textContent;
+    
+    segundoNumero=Number(pantalla.textContent);
+    
     if (operacion=='+'){
-        var resultado=suma(Number(primerNumero),Number(segundoNumero));
+        var resultado=suma (primerNumero,segundoNumero);
     }
      if (operacion=='-'){
-        resultado=resta(Number(primerNumero),Number(segundoNumero));
+        resultado=resta (primerNumero,segundoNumero);
      }
-        if (operacion=='*'){
-            resultado=multi(Number(primerNumero),Number(segundoNumero));
-        }
-            if (operacion=='/'){
-                resultado=dividir(Number(primerNumero),Number(segundoNumero));
-            }
-        borrar();
-        muestraValor(resultado);
-
-        }
+    if (operacion=='*'){
+        resultado=multi (primerNumero,segundoNumero);
+    }
+    if (operacion=='/'){
+        resultado=dividir (primerNumero,segundoNumero);
+    }
+    if (operacion =='raiz'){
+        resultado=raiz (primerNumero);
+    }
+    borrar();
+    muestraValor(resultado);
+}
 
 function resta (a,b){
     return a-b;
 }
-
 function suma (a,b){
     return a+b;
 }
@@ -49,5 +56,8 @@ function multi(a,b){
     return a*b;
 }
 function dividir(a,b){
-    return a/b;
+    return a/b.toFixed(5);
+}
+function raiz (a){
+    return Math.sqrt(a).toFixed(5);
 }
